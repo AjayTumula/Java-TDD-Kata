5401c0bdc063ade20b478b05db8a9a736a4f64c0
AjayTumula <ajaytumula72@gmail.com>
2023-02-07 20:57:27 +0000

    Testcases passing to convert 4, 36, and 54 to Roman Numerals

--- src/main/java/org/ajay/RomanNumeralsConverter.java
+++ src/main/java/org/ajay/RomanNumeralsConverter.java
@@ -4,9 +4,22 @@ public class RomanNumeralsConverter {
 
     public String convert(int arabicNumber) {
         String result = "";
-        for (int i = 0; i < arabicNumber; i++) {
-            result += "I";
+        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        final String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC","L", "XL","X", "IX", "V", "IV", "I"};
+        if(arabicNumber == 0 || arabicNumber > 3000){
+            result = "Not a roman numeral";
+        }
+        else {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < values.length; i++) {
+                while (arabicNumber >= values[i]) {
+                    arabicNumber -= values[i];
+                    sb.append(romanNumerals[i]);
+                }
+            }
+            result = sb.toString();
         }
         return result;
     }
+
 }
--- src/test/java/com/ajay/RomanNumeralsConverterTest.java
+++ src/test/java/com/ajay/RomanNumeralsConverterTest.java
@@ -11,22 +11,43 @@ public class RomanNumeralsConverterTest {
     public void ConvertOneToNumeral() {
         RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
 
-        assertEquals("I",romanNumeralsConverter.convert(1));
+        assertEquals("I", romanNumeralsConverter.convert(1));
     }
 
     @Test
     public void ConvertTwoToNumeral() {
         RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
 
-        assertEquals("II",romanNumeralsConverter.convert(2));
+        assertEquals("II", romanNumeralsConverter.convert(2));
     }
 
     @Test
     public void ConvertThreeToNumeral() {
         RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
 
-        assertEquals("III",romanNumeralsConverter.convert(3));
+        assertEquals("III", romanNumeralsConverter.convert(3));
+    }
+
+    @Test
+    public void ConvertFourToNumeral() {
+        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+
+        assertEquals("IV", romanNumeralsConverter.convert(4));
     }
 
 
+    @Test
+    public void ConvertThirtySixToNumeral() {
+        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+
+        assertEquals("XXXVI", romanNumeralsConverter.convert(36));
+    }
+
+
+    @Test
+    public void ConvertFiftyFourToNumeral() {
+        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+
+        assertEquals("LIV", romanNumeralsConverter.convert(54));
+    }
 }
